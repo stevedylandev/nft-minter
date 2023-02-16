4f6d0ccd1e77ac0fee9222d16f56f2c031868aae
Steve <stevedsimkins@gmail.com>
2023-02-16 11:50:35 -0500

    init

---
+++ src/pages/api/mint.js
@@ -0,0 +1,27 @@
+import { DefenderRelayProvider } from "defender-relay-client/lib/web3"
+import Web3 from "web3"
+import abi from "../utils/abi.json"
+
+const credentials = {apiKey: `${process.env.NEXT_PUBLIC_DEFENDER_KEY}`, apiSecret: `${process.env.NEXT_PUBLIC_DEFENDER_SECRET_KEY}`}
+const provider = new DefenderRelayProvider(credentials, { speed: 'fast' });
+//const signer = new DefenderRelaySigner(credentials, provider, { speed: 'fast' });
+const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+const web3 = new Web3(provider)
+
+export default async function handler(req, res) {
+  try {
+  const address = req.body.walletAddress.toString()
+  const uri = req.body.uri.toString()
+  console.log(address, uri)
+
+  const [from] = await web3.eth.getAccounts()
+  const contract = new web3.eth.Contract(abi.output.abi, contractAddress, { from })
+  const mintTxn = await contract.methods.safeMint(address, uri).send()
+  console.log(mintTxn)
+
+  res.status(200).json({ mintTxn })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
---
+++ src/pages/components/Form.js
@@ -0,0 +1,157 @@
+import { useState } from 'react'
+import axios from 'axios'
+import styles from '@/styles/Form.module.css'
+import RingLoader from "react-spinners/RingLoader"
+
+
+const JWT = `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`
+const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+
+const FileUpload = () => {
+  const [selectedFile, setSelectedFile] = useState()
+  const [name, setName] = useState()
+  const [description, setDescription] = useState()
+  const [externalURL, setExternalURL] = useState()
+  const [sendTo, setSendTo] = useState()
+  const [osLink, setOsLink] = useState("https://opensea.io")
+  const [isLoading, setIsLoading] = useState(false)
+  const [message, setMessage] = useState("")
+  const [isComplete, setIsComplete] = useState(false)
+
+  const fileChangeHandler = (event) => {
+    setSelectedFile(event.target.files[0])
+  }
+  const nameChangeHandler = (event) => {
+    setName(event.target.value)
+  }
+  const descriptionChangeHandler = (event) => {
+    setDescription(event.target.value)
+  }
+  const externalURLChangeHandler = (event) => {
+    setExternalURL(event.target.value)
+  }
+  const sendToChangeHandler = (event) => {
+    setSendTo(event.target.value)
+  }
+
+  const handleSubmission = async () => {
+    setIsLoading(true)
+    const formData = new FormData()
+
+    formData.append('file', selectedFile)
+
+    const metadata = JSON.stringify({
+      name: '${selectedFile}',
+    })
+    formData.append('pinataMetadata', metadata)
+
+    const options = JSON.stringify({
+      cidVersion: 0,
+    })
+    formData.append('pinataOptions', options)
+
+    try {
+      setMessage("Uploading File...")
+      const uploadRes = await axios.post(
+        'https://api.pinata.cloud/pinning/pinFileToIPFS',
+        formData,
+        {
+          maxBodyLength: 'Infinity',
+          headers: {
+            'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+            Authorization: JWT,
+          },
+        }
+      )
+      console.log(uploadRes.data)
+      const hash = uploadRes.data.IpfsHash
+
+      const jsonData = JSON.stringify({
+        name: name,
+        description: description,
+        image: `https://discordpinnie.mypinata.cloud/ipfs/${hash}`,
+        external_url: externalURL
+      })
+      setMessage("Uploading Metadata...")
+      const jsonRes = await axios.post("https://api.pinata.cloud/pinning/pinJSONToIPFS", jsonData, {
+          maxBodyLength: 'Infinity',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: JWT,
+          },
+      })
+      const uri = jsonRes.data.IpfsHash
+
+      const mintBody = {
+        walletAddress: sendTo,
+        uri: `https://discordpinnie.mypinata.cloud/ipfs/${uri}`
+      }
+
+      setMessage("Minting NFT...")
+      const mintRes = await axios.post("/api/mint", mintBody)
+      console.log(mintRes)
+      
+      setOsLink(`https://testnets.opensea.io/assets/goerli/${contractAddress}/${mintRes.data.mintTxn.events.Transfer.returnValues.tokenId}`)
+      setMessage("Minting Complete! Click the link below to see your NFT")
+      setIsLoading(false)
+      setIsComplete(true)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+
+  return (
+    <div className={styles.form}>
+      {!isLoading && !isComplete && (
+        <>
+      <label className={styles.formInput} onChange={fileChangeHandler} htmlFor="file">
+        <input name="" type="file" id="file" hidden />
+        <p>{!selectedFile ? "Select File" : `${selectedFile.name}`}</p>
+      </label>
+      <label>Name</label>
+      <input type='text' placeholder='Cool NFT' onChange={nameChangeHandler} />
+      <label>Description</label>
+      <input
+        type='text'
+        placeholder='This NFT is just so cool'
+        onChange={descriptionChangeHandler}
+      />
+      <label>Your Website</label>
+      <input
+        type='text'
+        placeholder='https://pinata.cloud'
+        onChange={externalURLChangeHandler}
+      />
+      <label>Wallet Address</label>
+      <input
+        type='text'
+        placeholder='0x...'
+        onChange={sendToChangeHandler}
+      />
+      <button onClick={handleSubmission}>Submit</button>
+      </>
+      )}
+      {isLoading && (
+        <div className={styles.form}>
+          <RingLoader
+            loading={isLoading}
+            size={200}
+            aria-label="loading spinner"
+            color="#CC007E"
+            />
+          <h2>{message}</h2>
+        </div>
+      )}
+      {isComplete && (
+        <div className={styles.complete}>
+          <h3>{message}</h3>
+          <a href={osLink} target="_blank" rel="noreferrer">{osLink}</a>
+          <button onClick={() => setIsComplete(false) }>Mint Another NFT</button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default FileUpload
