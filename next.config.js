c99f3879d481cd9a0c20600a8cdec2a48c4b39c6
Steve <stevedsimkins@gmail.com>
2023-09-05 16:39:34 -0400

    mmm

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  apiHeaders: {
+    'Content-Length': 10 * 1024 * 1024, // 10MB
+  },
 }
 
 module.exports = nextConfig
