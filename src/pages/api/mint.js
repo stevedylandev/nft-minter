9c52973eeb71c932dc0ff8c938888b3ed9d2f62d
Steve <stevedsimkins@gmail.com>
2023-09-07 00:44:38 -0400

    removed uncessessary code

--- src/pages/api/mint.js
+++ src/pages/api/mint.js
@@ -2,51 +2,49 @@ const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const data = JSON.stringify({
-        recipient: `polygon:${req.body.address}`,
-        metadata: req.body.uri,
-      })
+  try {
+    const data = JSON.stringify({
+      recipient: `polygon:${req.body.address}`,
+      metadata: req.body.uri,
+    })
 
-      const mintRes = await fetch(`https://www.crossmint.com/api/2022-06-09/collections/${process.env.CROSSMINT_COLLECTION_ID}/nfts`, {
-        method: 'POST',
-        headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
-          'x-client-secret': `${process.env.CROSSMINT_CLIENT_SECRET}`,
-          'x-project-id': `${process.env.CROSSMINT_PROJECT_ID}`
-        },
-        body: data
-      })
+    const mintRes = await fetch(`https://www.crossmint.com/api/2022-06-09/collections/${process.env.CROSSMINT_COLLECTION_ID}/nfts`, {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        'x-client-secret': `${process.env.CROSSMINT_CLIENT_SECRET}`,
+        'x-project-id': `${process.env.CROSSMINT_PROJECT_ID}`
+      },
+      body: data
+    })
 
-      const mintResJson = await mintRes.json()
+    const mintResJson = await mintRes.json()
 
-      if (mintResJson.onChain.status === "pending") {
-        while (true) {
-          delay(5000)
+    if (mintResJson.onChain.status === "pending") {
+      while (true) {
+        delay(5000)
 
-          const mintStatus = await fetch(`https://www.crossmint.com/api/2022-06-09/collections/${process.env.CROSSMINT_COLLECTION_ID}/nfts/${mintResJson.id}`, {
-            method: 'GET',
-            headers: {
-              accept: 'application/json',
-              'x-client-secret': `${process.env.CROSSMINT_CLIENT_SECRET}`,
-              'x-project-id': `${process.env.CROSSMINT_PROJECT_ID}`
-            }
-          })
-
-          const mintStatusJson = await mintStatus.json()
-
-          if (mintStatusJson.onChain.status === "success") {
-            res.status(200).json(mintStatusJson)
-            return
+        const mintStatus = await fetch(`https://www.crossmint.com/api/2022-06-09/collections/${process.env.CROSSMINT_COLLECTION_ID}/nfts/${mintResJson.id}`, {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            'x-client-secret': `${process.env.CROSSMINT_CLIENT_SECRET}`,
+            'x-project-id': `${process.env.CROSSMINT_PROJECT_ID}`
           }
+        })
+
+        const mintStatusJson = await mintStatus.json()
+
+        if (mintStatusJson.onChain.status === "success") {
+          res.status(200).json(mintStatusJson)
+          return
         }
       }
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ text: "Error minting NFT", error: error })
     }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ text: "Error minting NFT", error: error })
   }
 }
 
